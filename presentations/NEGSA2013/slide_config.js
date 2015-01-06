7b5532133b925259eb8a9a85a0f8de9793e6510d
John Van Hoesen <vanhoesenj@gmail.com>
2015-01-05 20:10:50 -0500

    update presentations

---
+++ presentations/NEGSA2013/slide_config.js
@@ -0,0 +1,36 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Leveraging a GIS and The Emerging GeoWeb',
+    subtitle: 'Towards Improving the Communication of Geologic Mapping Endeavors to Community Stakeholders',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/gsa.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'John Van Hoesen',
+    company: 'Associate Professor, Green Mountain College',
+    twitter: '@taconic_musings',
+    www: 'http://www.greenmtn.edu/vanhoesenj.aspx',
+	email: 'vanhoesenj@greenmtn.edu',
+    github: 'http://github.com/vanhoesenj'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
